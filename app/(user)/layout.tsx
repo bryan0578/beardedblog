3f0aa7dc6227f09c9f774f447bc7445925c5d297
Bryan <bryan.cash78@gmail.com>
2023-11-20 20:27:14 -0500

    added env for studio preview and metadata

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -9,6 +9,14 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Bearded Blog',
   description: 'BeardedCash Blog website where you can find articles related to web and app design, and development.',
+  other: {
+    "twitter:image": 'https://i.ibb.co/BGkVHpN/Beard-Logo-Orange-8.png',
+    "twitter:card": "summary_large_image",
+    "twitter:site": "@beardedcash",
+    "og:url": "beardedblog.vercel.app",
+    "og:image": 'https://i.ibb.co/BGkVHpN/Beard-Logo-Orange-8.png',
+    "og:type": "website",
+  }
 }
 
 export default function RootLayout({
